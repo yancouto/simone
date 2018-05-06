1baf00cc2673d158353aa20a8861d7e8ccc268f3
Pedro Sousa <pedroteosousa@gmail.com>
2018-05-06 07:24:33 -0300

    Fixed filterContests

--- src/utils/filterContests.js
+++ src/utils/filterContests.js
@@ -3,14 +3,18 @@ import type { Contest } from '../types';
 
 export function filterContests(contests: Array<Contest>, from?: Date, to?: Date): Array<Contest> {
   contests = contests.filter((contest) => {
-    if (from instanceof Date && contest.startTime instanceof Date)
-      return contest.startTime >= from;
-    else return false;
+    if (from instanceof Date)
+      if (contest.startTime instanceof Date)
+        return contest.startTime >= from;
+      else return false;
+    else return true;
   });
   contests = contests.filter((contest) => {
-    if (to instanceof Date && contest.startTime instanceof Date)
-      return contest.startTime <= to;
-    else return false;
+    if (to instanceof Date)
+      if (contest.startTime instanceof Date)
+        return contest.startTime <= to;
+      else return false;
+    else return true;
   });
   return contests;
 }
